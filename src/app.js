7797d5f0c60eef9f14b3966385243386f7219c9d
Kima93 <154838127+Kima93@users.noreply.github.com>
2025-02-15 19:22:25 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  // array with the words
+  
   let first = "A ";
   let adj = ["two headed ", "nuclear ", "angry ", "lonely ", "crazy ", "glowing ", "smelly ", "slow ", "old "];
   let noun = ["jogger ", "racoon ", "dog ", "merchant ", "driver ", "comedian ", "pinecone "];
@@ -7,14 +7,14 @@ window.onload = function() {
   let possetion = ["toe ", "car ", "watch ", "shoe ", "wallet ", "shirt ", "keys ", "computer ", "phone ", "sandwich "];
   let where = ["on the street", "in my house", "in my driveway", "in front of the office", "near the mall", "near the toilet", "at the bus station"];
 
-  // declaring random variables
+ 
   let rdm1 = Math.floor(Math.random() * adj.length);
   let rdm2 = Math.floor(Math.random() * noun.length);
   let rdm3 = Math.floor(Math.random() * action.length);
   let rdm4 = Math.floor(Math.random() * possetion.length);
   let rdm5 = Math.floor(Math.random() * where.length);
 
-  // creating a sentence (the excuse)
+  
   document.querySelector("#excuse").innerHTML = first + adj[rdm1] + noun[rdm2] + action[rdm3] + possetion[rdm4] + where[rdm5];
   
 }
